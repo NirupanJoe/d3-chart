baaca61e6aa57f94bc5f19c01fd703dfa8696c7e
njj <njj@laufire.in>
2022-06-07 12:26:17 +0530

    Change a custom color for rechart pie.

--- src/components/pie/reChartPie.js
+++ src/components/pie/reChartPie.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { PieChart, Pie, Tooltip, Legend, LabelList } from 'recharts';
+import { PieChart, Pie, Tooltip, Legend, LabelList, Cell } from 'recharts';
+// eslint-disable-next-line id-match
+import * as d3 from 'd3';
 
+// eslint-disable-next-line max-lines-per-function
 const ReChartPie = (context) => {
 	const { config: { pieChartProps: {
 		width, height, radius: { inner, outer }, pieFill, labelFill,
 	}}, data } = context;
+	const colors = d3.scaleOrdinal(d3.schemeDark2);
 
 	return (
 		<PieChart { ...{ width, height } }>
@@ -15,6 +19,8 @@ const ReChartPie = (context) => {
 				outerRadius={ outer }
 				innerRadius={ inner }
 			>
+				{data.map((dummy, key) =>
+					<Cell key={ key } fill={ colors(key) }/>)}
 				<LabelList fill={ labelFill }/>
 			</Pie>
 			<Tooltip/>
